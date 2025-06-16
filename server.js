593ea7f48c604e609174d3b03fcfd2e383cfd812
Thekra <thekra.aljagthmi@com>
2025-06-16 13:34:51 +0300

    Prevent post requests on this server

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const port = 3001;
 
+app.use(express.static('public'));
 
 // Get - redirect from root to /hello-world
 app.get('/', (req, res) => {
-  res.redirect('/hello-world')
-})
+  res.redirect('/hello-world');
+});
 
 // redirect to /hello-world from /hello-world.json
 app.get('/hello-world', (req, res) => {
@@ -15,7 +16,7 @@ app.get('/hello-world', (req, res) => {
 
 app.get('/hello-world.json', (req, res) => {
   res.json({});
-}); 
+});
 
 app.get('/hello-world.png', (req, res) => {
   res.json({});
@@ -26,15 +27,6 @@ app.use((req, res) => {
   res.status(404).send(`${req.method} is not supported on ${req.path}`);
 });
 
-// Post requests are not allowed on this server 
-app.use( (req, res) => {
-  if (req.method === 'POST') {
-    return res.status(405).send(`POST is not allowed on ${res.path}`);
-  }
-});
-
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
